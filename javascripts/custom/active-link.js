a37c1eafbc759b014381918a72e70c3d8468b14d
snollygolly <snollygolly@gmail.com>
2016-04-15 07:54:14 -0500

    made index show active when on root page

--- javascripts/custom/active-link.js
+++ javascripts/custom/active-link.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-  if (location.pathname != "/modules")
+  if (location.pathname == "/t2-start/") {
+    var navLink = "a[href*='" + location.pathname + "index.html']";
+  } else {
     var navLink = "a[href*='" + location.pathname + "']";
-    $(navLink).addClass("currentlink");
-    $(navLink).parent("li").addClass("active");
+  }
+  $(navLink).addClass("currentlink");
+  $(navLink).parent("li").addClass("active");
 });
